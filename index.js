9966535c75557ecd7a2300391466490ee55bf606
naomi curapil <curapilnaomi@icloud.com>
2021-11-06 17:51:44 -0500

    commit tree 1cc067c873305088956380df1e03af4e687993d8

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+
+function findMatching(driver, name) {
+    return driver.filter( wheel => wheel.toLowerCase() === name.toLowerCase())
+};
+
+
+
+function fuzzyMatch(letters, words){
+    return letters.filter( fans => fans.indexOf(words) === 0 )
+
+}
+      
+function matchName(city,area){
+    return city.filter( ferrisWheel => ferrisWheel.name === area )
+}
